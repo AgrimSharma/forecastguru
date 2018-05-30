b340d9ab1edffebf2d28e5ca7c3ab80732719776
agrim <agrim.sharma@sirez.com>
2018-05-30 21:21:42 +0530

    updated

--- alpha/views.py
+++ alpha/views.py
@@ -488,9 +488,10 @@ def my_forecast(request):
     except Exception:
         return render(request, 'my_friend_nl.html', {"user": request.user.username})
 
-    return render(request, 'my_friend.html', {"live": live_forecast_data(account),
-                                              "result": forecast_result_data(account),
-                                              "user": request.user.username})
+    return render(request, 'my_friend.html', {
+        # "live": live_forecast_data(account),
+        #                                       "result": forecast_result_data(account),
+        "profile": account,"user": request.user.username})
 
 
 def logout_view(request):
