c5a6dbe749a36bd97d8567b5bbc31ffb34dfac9b
agrim <agrim.sharma@sirez.com>
2018-06-06 17:32:23 +0530

    updated

--- alpha/views.py
+++ alpha/views.py
@@ -285,7 +285,7 @@ def profile(request):
 
     total = profile.fg_points_free + profile.market_fee + profile.fg_points_won + profile.fg_points_bought - profile.fg_points_lost - profile.market_fee_paid - point
     profile.fg_points_total = total
-    totals = profile.successful_forecast + profile.unsuccessful_forecast
+    totals = Betting.objects.filter(users=profile).count()
     try:
         suc_per = (profile.successful_forecast / totals) * 100
         unsuc_per = 100 - suc_per
