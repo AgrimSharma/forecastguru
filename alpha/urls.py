40781ebc37d56134b52fb6195656800584660d35
agrim <agrim.sharma@sirez.com>
2018-06-07 10:59:32 +0530

    updated

--- alpha/urls.py
+++ alpha/urls.py
@@ -50,6 +50,8 @@ urlpatterns = [
     url(r'^closed_status/', update_close_status, name="closed_status"),
     url(r'^terms/', terms, name="terms"),
     url(r'^faq/', faq, name="faq"),
+    url(r'^proc/', profile_view, name="profile_view"),
+
     url(r'^', main_page, name='main_page'),
 
 ]
