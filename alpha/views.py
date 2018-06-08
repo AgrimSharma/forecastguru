92c697315f3f46d57678f3a079e960a281571f1e
agrim <agrim.sharma@sirez.com>
2018-06-08 17:31:52 +0530

    updated

--- alpha/views.py
+++ alpha/views.py
@@ -1287,7 +1287,7 @@ def facebook_category(request):
 
 
 def session(request):
-    return HttpResponse(request.session.__dict__)
+    return HttpResponse(json.dumps(request.session.__dict__))
 
 
 def main_page(request):
