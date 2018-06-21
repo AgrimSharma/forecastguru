3750dee4a646cab2a6b8bf63aefb77c1b29f42f2
agrim <agrim.sharma@sirez.com>
2018-06-21 17:41:23 +0530

    updated

--- alpha/views.py
+++ alpha/views.py
@@ -1504,15 +1504,11 @@ def my_forecast_private(request):
         user = request.user.id
         users = User.objects.get(id=user)
         account = SocialAccount.objects.get(user=users)
-        forecast_live = ForeCast.objects.filter(approved__name="yes", status__name='In-Progress',
-                                                user=account, private__name='yes').order_by("expire")
-
         forecast_result = ForeCast.objects.filter(approved__name="yes", status__name='Result Declared',
                                                   user=account, private__name='yes').order_by("expire")
         forecast_approval = InviteFriends.objects.filter(user=account).order_by("-forecast__expire")
 
-        return render(request, 'my_friend_private.html', {"live": live_forecast_data_private(forecast_live, account),
-                                                          "result": forecast_result_data_private(forecast_result, account),
+        return render(request, 'my_friend_private.html', {"result": forecast_result_data_private(forecast_result, account),
                                                           "approval": forecast_invite_data(forecast_approval, account),
                                                           "user": "Guest" if request.user.is_anonymous() else request.user.username,
                                                           "heading": "Forecast Private",
