ed91b97d046d9376a71ec4590da4b9be18c33050
agrim <agrim.sharma@sirez.com>
2018-06-21 18:27:45 +0530

    updated

--- alpha/urls.py
+++ alpha/urls.py
@@ -19,6 +19,7 @@ from .views import *
 urlpatterns = [
     url(r'^accounts/profile/', test, name='home_page'),
     url(r'^create_forecast/', create_forecast, name='create_forecast'),
+    url(r'^trending/', trending_forecast, name='trending'),
     url(r'^result_declared/', forecast_result, name='forecast_result'),
     url(r'^result_not_declared/', result_not_declared, name='result_not_declared'),
     url(r'^closing_soon/', closing_soon, name='closing_soon'),
