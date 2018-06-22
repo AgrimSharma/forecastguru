8fb74112c6759b96be2be6dddfb3afc30e326660
agrim <agrim.sharma@sirez.com>
2018-06-22 21:40:12 +0530

    updated

--- alpha/views.py
+++ alpha/views.py
@@ -1565,7 +1565,7 @@ def my_forecast_private(request):
         forecast_live = ForeCast.objects.filter(approved__name="yes", status__name='In-Progress',user=account, private__name='yes').order_by("expire")
 
         forecast_result = ForeCast.objects.filter(approved__name="yes", status__name='Result Declared',user=account, private__name='yes').order_by("expire")
-        forecast_approval = InviteFriends.objects.filter(user=account).order_by("-forecast__expire")
+        forecast_approval = InviteFriends.objects.filter(user=account).exclude(forecast__in=forecast_result).exclude(forecast__in=forecast_live).order_by("-forecast__expire")
 
         return render(request, 'my_friend_private.html', {"live": live_forecast_data_private(forecast_live, account),
                                                           "result": forecast_result_data_private(forecast_result,
