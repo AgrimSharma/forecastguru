de949d935ba120368863fce45de0e58b0a753a19
agrim <agrim.sharma@sirez.com>
2018-06-28 16:05:59 +0530

    updated

--- alpha/admin.py
+++ alpha/admin.py
@@ -69,7 +69,6 @@ class StatusAdmin(admin.ModelAdmin):
 class BettingAdmin(admin.ModelAdmin):
 
     list_display = ['get_forecast', "get_forecast_category", "get_forecast_sub_category", 'users', 'bet_for', 'bet_against']
-    change_form_template = 'change_list.html'
     date_hierarchy = 'forecast__expire'
     search_fields = ['forecast__heading']
     list_filter = ("forecast__category", )
@@ -96,10 +95,21 @@ class OrderAdmin(admin.ModelAdmin):
     list_filter = ('user', 'amount', 'order_date')
 
 
-# admin.site.register(Source, SourceAdmin)
+class NotificationUserAdmin(admin.ModelAdmin):
+    list_display = ['user', 'subscriber_id']
+    search_fields = ['user']
+
+
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ['user', 'title', "message", "url", "status"]
+    list_filter = ('status',)
+
+
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(SubCategory, SubCategoryAdmin)
 admin.site.register(ForeCast, ForeCastAdmin)
+admin.site.register(NotificationUser, NotificationUserAdmin)
+admin.site.register(NotificationPanel, NotificationAdmin)
 admin.site.register(Status, StatusAdmin)
 admin.site.register(Betting, BettingAdmin)
 admin.site.register(Banner, BannerAdmin)
