2c6dd1dc7893e852e7c85cfc101b6440833ea1dc
agrim <agrim.sharma@sirez.com>
2018-07-12 10:05:35 +0530

    updated

--- alpha/views.py
+++ alpha/views.py
@@ -2442,7 +2442,7 @@ def fifa_rounds(request):
         return render(request, 'category_search_fifa.html',
                       {
                           "live": forecast_live_fifa(forecast_live, profile),
-                          "heading": "Fifa Semi-Finals",
+                          "heading": "Fifa World Cup Finals",
                           "title": "ForecastGuru",
                           "user": "Guest" if request.user.is_anonymous() else request.user.username
                       })
